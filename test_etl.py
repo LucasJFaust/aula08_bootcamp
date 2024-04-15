904766e9305cb963319ce3cce2885d1992914133
LucasJFaust <lucasjfm.eng@gmail.com>
2024-04-14 21:27:16 -0300

    Finalizando a etl

---
+++ test_etl.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+    pasta = 'data'
+    print(extrair_dados(path=pasta))
+
+            Produto    Categoria  Quantidade  Venda        Data
+0    Notebook Gamer  Eletrônicos           7   1500  2023-01-16
+1     Mouse Sem Fio  Eletrônicos          10     30  2023-01-16
+2  Teclado Mecânico  Eletrônicos           3    100  2023-01-16
+3    Notebook Gamer  Eletrônicos           2   1500  2023-01-17
+4     Mouse Sem Fio  Eletrônicos          14     30  2023-01-17
+5  Teclado Mecânico  Eletrônicos           2    100  2023-01-17
+6    Notebook Gamer  Eletrônicos           3   1500  2023-01-15
+7     Mouse Sem Fio  Eletrônicos          10     30  2023-01-15
+8  Teclado Mecânico  Eletrônicos           5    100  2023-01-15
